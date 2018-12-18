7f8408d8588a53f0f51f5b8896e04e834a569e39
ashyongwong <jywong-wa16@student.tarc.edu.my>
2018-12-18 13:29:25 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/taruc/lab2_2limplicitintent/MainActivity.java
@@ -0,0 +1,13 @@
+package my.edu.taruc.lab2_2limplicitintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
